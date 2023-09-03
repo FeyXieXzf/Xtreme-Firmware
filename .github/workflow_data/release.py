1174c208791309e4607cdcb585a646a64b1b0ed9
Ujhhgtg <feyxiexzf@gmail.com>
2023-09-03 17:45:59 +0800

    [ci] i probably don't need this

--- .github/workflow_data/release.py
+++ .github/workflow_data/release.py
@@ -4,15 +4,15 @@ import os
 
 if __name__ == "__main__":
     notes_path = '.github/workflow_data/release.md'
-    with open(os.environ['GITHUB_EVENT_PATH'], "r") as f:
-        changelog = json.load(f)['pull_request']['body']
+    #with open(os.environ['GITHUB_EVENT_PATH'], "r") as f:
+    #    changelog = json.load(f)['pull_request']['body']
     with open(notes_path, "r") as f:
         template = f.read()
     notes = template.format(
         ARTIFACT_TGZ=os.environ['ARTIFACT_TGZ'],
         ARTIFACT_ZIP=os.environ['ARTIFACT_ZIP'],
         VERSION_TAG=os.environ['VERSION_TAG'],
-        CHANGELOG=changelog
+        CHANGELOG=""
     )
     with open(notes_path, "w") as f:
         f.write(notes)
